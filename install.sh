c513a0ae5e50d084c15ffa5ba0669a5080999e79
Chris Palmer <chris@noncombatant.org>
2015-03-21 21:53:26 -0700

    Simplify.

--- install.sh
+++ install.sh
@@ -3,42 +3,14 @@
 rc_files="bashrc gitconfig Xresources muttrc vimrc"
 home_bin="$HOME/bin"
 
-install() {
-    mkdir -p "$home_bin"
+mkdir -p "$home_bin"
 
-    for i in $rc_files
-    do
-        cp -i -a "$i" "$HOME"/."$i"
-    done
+for i in $rc_files
+do
+    cp -i -a "$i" "$HOME"/."$i"
+done
 
-    for i in bin/*
-    do
-        cp -i -a "$i" "$home_bin"/"$(basename $i)"
-    done
-}
-
-slurp() {
-    for i in $rc_files
-    do
-        cp -f -a "$HOME"/."$i" "$i"
-    done
-
-    for i in bin/*
-    do
-        cp -f -a "$home_bin"/"$(basename $i)" "$i"
-    done
-
-    git status
-}
-
-case "$1" in 
-    install)
-        install
-        ;;
-    slurp)
-        slurp
-        ;;
-    *)
-        echo "Usage: $0 {install|slurp}"
-        exit 1
-esac
+for i in bin/*
+do
+    cp -i -a "$i" "$home_bin"/"$(basename $i)"
+done
