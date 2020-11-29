ea649a7573245b15e0f693c72a82ee84adaf56f3
Chris Palmer <chris@noncombatant.org>
2020-11-29 13:28:00 -0800

    Check before overwriting changes.

--- install.sh
+++ install.sh
@@ -6,5 +6,9 @@ for file in *; do
   if test ! -f "$file" -o "$self" = "$file"; then
     continue
   fi
+  if ! cmp "$file" "$HOME/.$file"; then
+    diff "$file" "$HOME/.$file"
+    exit 1
+  fi
   cp "$file" "$HOME/.$file"
 done
