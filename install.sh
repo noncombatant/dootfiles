3a840fc9f19edc18577e1659223ca237a7088a36
Chris Palmer <chris@noncombatant.org>
2021-08-28 14:49:17 -0700

    Add bin/ and some scripts.
    And install them.
    
    Also further robustify the install.sh script.

--- install.sh
+++ install.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
 
+set -e
+
+if test -z "$HOME"; then
+  echo "No \$HOME directory set." > /dev/stderr
+  exit 1
+fi
+
+if test ! -d "$HOME"; then
+  echo "'$HOME' is not a directory." > /dev/stderr
+  exit 1
+fi
+
 here=$(dirname "$0")
 
 for file in $here/doots/*; do
   f=$(basename "$file")
   cp "$file" "$HOME/.$f"
+done
+
+bin="$HOME/bin"
+mkdir -p "$bin"
+
+for file in $here/bin/*; do
+  f=$(basename "$file")
+  cp "$file" "$bin"
 done
