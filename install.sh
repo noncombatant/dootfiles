5c03af85aef932f1a4a2c8401fdccb15b290b05c
Chris Palmer <chris@noncombatant.org>
2021-09-05 14:40:42 -0700

    Auto-install all the modern goodies.
    From source.

--- install.sh
+++ install.sh
@@ -26,3 +26,48 @@ for file in $here/bin/*; do
   f=$(basename "$file")
   cp "$file" "$bin"
 done
+
+# Install the modern goodies:
+
+mkdir foo
+
+cd foo
+
+# bat
+git clone https://github.com/sharkdp/bat.git
+cd bat
+cargo install --locked bat
+which bat
+cd ..
+
+# fd
+git clone https://github.com/sharkdp/fd.git
+cd fd
+cargo install fd-find
+which fd
+cd ..
+
+# ripgrep
+git clone https://github.com/BurntSushi/ripgrep.git
+cd ripgrep
+cargo install ripgrep
+which rg
+cd ..
+
+# fzf
+git clone https://github.com/junegunn/fzf.git
+cd fzf
+make install
+cp bin/fzf ~/bin/fzf
+which fzf
+cd ..
+
+# search
+git clone https://github.com/noncombatant/search.git
+cd search
+go build
+mv search ~/bin
+which search
+
+cd ..
+rm -rf foo
