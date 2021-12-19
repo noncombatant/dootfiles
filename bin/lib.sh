f1b72aee0fdd3f1e4ca426e89db21fbdceb743a0
Chris Palmer <chris@noncombatant.org>
2021-12-18 23:31:18 -0800

    Add `integrity`

--- bin/lib.sh
+++ bin/lib.sh
@@ -1,3 +1,13 @@
 udate() {
   date -u +'%Y-%m-%d-%H:%M:%S'
 }
+
+core_count() {
+  sysctl -n hw.physicalcpu 2> /dev/null || \
+  grep -c ^processor /proc/cpuinfo 2> /dev/null || \
+  echo 1
+}
+
+realpath() {
+  echo "$(cd "$(dirname "$1")" && pwd -P)/$(basename "$1")"
+}
