125cdad0553834e6d700a6935f9a2eab08d8ba06
Chris Palmer <chris@noncombatant.org>
2025-02-08 12:34:38 -0800

    `#ifdef` the platform-specific clocks

--- cantrips/clocks.c
+++ cantrips/clocks.c
@@ -5,21 +5,93 @@
 #include "utils.h"
 
 static clockid_t clocks[] = {
-    CLOCK_REALTIME,           CLOCK_MONOTONIC,
-    CLOCK_MONOTONIC_RAW,      CLOCK_MONOTONIC_RAW_APPROX,
-    CLOCK_UPTIME_RAW,         CLOCK_UPTIME_RAW_APPROX,
-    CLOCK_PROCESS_CPUTIME_ID, CLOCK_THREAD_CPUTIME_ID,
+#ifdef CLOCK_REALTIME
+    CLOCK_REALTIME,
+#endif
+#ifdef CLOCK_REALTIME_ALARM
+    CLOCK_REALTIME_ALARM,
+#endif
+#ifdef CLOCK_REALTIME_COARSE
+    CLOCK_REALTIME_COARSE,
+#endif
+#ifdef CLOCK_TAI
+    CLOCK_TAI,
+#endif
+#ifdef CLOCK_MONOTONIC
+    CLOCK_MONOTONIC,
+#endif
+#ifdef CLOCK_MONOTONIC_COARSE
+    CLOCK_MONOTONIC_COARSE,
+#endif
+#ifdef CLOCK_MONOTONIC_RAW
+    CLOCK_MONOTONIC_RAW,
+#endif
+#ifdef CLOCK_MONOTONIC_RAW_APPROX
+    CLOCK_MONOTONIC_RAW_APPROX,
+#endif
+#ifdef CLOCK_BOOTTIME
+    CLOCK_BOOTTIME,
+#endif
+#ifdef CLOCK_BOOTTIME_ALARM
+    CLOCK_BOOTTIME_ALARM,
+#endif
+#ifdef CLOCK_UPTIME_RAW
+    CLOCK_UPTIME_RAW,
+#endif
+#ifdef CLOCK_UPTIME_RAW_APPROX
+    CLOCK_UPTIME_RAW_APPROX,
+#endif
+#ifdef CLOCK_PROCESS_CPUTIME_ID
+    CLOCK_PROCESS_CPUTIME_ID,
+#endif
+#ifdef CLOCK_THREAD_CPUTIME_ID
+    CLOCK_THREAD_CPUTIME_ID,
+#endif
 };
 
 static char* clock_names[] = {
+#ifdef CLOCK_REALTIME
     [CLOCK_REALTIME] = "realtime",
+#endif
+#ifdef CLOCK_REALTIME_ALARM
+    [CLOCK_REALTIME_ALARM] = "realtime alarm",
+#endif
+#ifdef CLOCK_REALTIME_COARSE
+    [CLOCK_REALTIME_COARSE] = "realtime coarse",
+#endif
+#ifdef CLOCK_TAI
+    [CLOCK_TAI] = "TAI",
+#endif
+#ifdef CLOCK_MONOTONIC
     [CLOCK_MONOTONIC] = "monotonic",
+#endif
+#ifdef CLOCK_MONOTONIC_COARSE
+    [CLOCK_MONOTONIC_COARSE] = "monotonic coarse",
+#endif
+#ifdef CLOCK_MONOTONIC_RAW
     [CLOCK_MONOTONIC_RAW] = "monotonic (raw)",
+#endif
+#ifdef CLOCK_MONOTONIC_RAW_APPROX
     [CLOCK_MONOTONIC_RAW_APPROX] = "monotonic (raw, approximate)",
+#endif
+#ifdef CLOCK_BOOTTIME
+    [CLOCK_BOOTTIME] = "boot time",
+#endif
+#ifdef CLOCK_BOOTTIME_ALARM
+    [CLOCK_BOOTTIME_ALARM] = "boot time alarm",
+#endif
+#ifdef CLOCK_UPTIME_RAW
     [CLOCK_UPTIME_RAW] = "uptime (raw)",
+#endif
+#ifdef CLOCK_UPTIME_RAW_APPROX
     [CLOCK_UPTIME_RAW_APPROX] = "uptime (raw, approximate)",
+#endif
+#ifdef CLOCK_PROCESS_CPUTIME_ID
     [CLOCK_PROCESS_CPUTIME_ID] = "process CPU time",
+#endif
+#ifdef CLOCK_THREAD_CPUTIME_ID
     [CLOCK_THREAD_CPUTIME_ID] = "thread CPU time",
+#endif
 };
 
 int main() {
